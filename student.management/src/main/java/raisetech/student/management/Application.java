c14707396e469c1f6a25d8b0f0bd42dde879a31a
Yusuke2000 <lemonsawarlove@gmail.com>
2026-04-12 20:48:50 +0900

    first commit

---
+++ student.management/src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,24 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "一貫入魂！Spring Boot始めました！";
+	}
+}
+
+
+
